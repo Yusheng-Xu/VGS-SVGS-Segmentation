8d67764ca0dcd4a424ee1d05266d4ea45d624830
Yusheng Xu <xysavenger@gmail.com>
2017-01-04 15:01:23 +0100

    Add files via upload

---
+++ voxel_segmentation.hpp
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+//	File:		 voxel_segmentation.hpp
+//	Author:		 Yusheng Xu, PF_Technische Universitaet Muechen (yusheng.xu@tum.de)
+//	Description: The voxel based segmentation for point cloud 
+//  Modified:    14.5.2016
+//
+//  Copyright (c) 2016  Yusheng Xu (yusheng.xu@tum.de)
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation; either
+//  Version 3 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  General Public License for more details.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef PCL_SEGMENTATION_VOXEL_IMPL_H_
+#define PCL_SEGMENTATION_VOXEL_IMPL_H_
+
+
+#include <pcl/octree/octree.h>
+#include <pcl/octree/octree_pointcloud.h>
+#include <pcl/octree/octree_iterator.h>
+#include <pcl/octree/octree_impl.h>
+
+#endif // PCL_SEGMENTATION_VOXEL_H_
