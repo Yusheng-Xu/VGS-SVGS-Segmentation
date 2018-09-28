ed4b54ea4ae59012f4d4c0f9a63490b7fb7892c1
Yusheng Xu <xysavenger@gmail.com>
2018-09-28 09:47:13 +0200

    Update point_clouds_IO.h

--- point_clouds_IO.h
+++ point_clouds_IO.h
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
-//	File:		 point_cloud_IO.h
-//	Author:		 Yusheng Xu, PF_Technische Universitaet Muechen (yusheng.xu@tum.de)
-//	Description: IO operation of the point clouds
+//  File:        point_cloud_IO.h
+//  Author:	 Yusheng Xu, PF_Technische Universitaet Muechen (yusheng.xu@tum.de)
+//  Description: IO operation of the point clouds
 //  Modified:    29.7.2016
 //
 //  Copyright (c) 2015-2017  Yusheng Xu (yusheng.xu@tum.de)
@@ -89,7 +89,6 @@ inputPointCloudData2(string dataName, PTTypePtr dataCloud)
 		PCL_ERROR("Couldn't read the PLY file!");
 		return(-1);
 	}
-
 	//Show the information of the points in the input PLY files
 	size_t dataSize = dataCloud->points.size();  // Size of the point clouds
 	return (0);
@@ -105,21 +104,9 @@ outputPointCloudData(string outName, PTTypePtr dataCloud)
 		PCL_ERROR("Couldn't save the PCD file!");
 		return(-1);
 	}
-	
 	return (0);
 }
 
-//IO::saveTxT
-void
-saveTxtPoints(string path_name, pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud);
-
-//IO::save mesh consisting of colored lines
-void
-saveLinesOfMesh(string path_name, pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud);
-
-//IO::save lines representing normal vectors
-void
-saveNormalVectors(string pathname_file, pcl::PointCloud<pcl::Normal>::Ptr input_normals, pcl::PointCloud<pcl::PointXYZ>::Ptr input_points, float length_normal);
 
 //IO::save colored clusters in a point cloud
 void
@@ -132,26 +119,7 @@ void
 showColoredClusters(string fileoutpath_name, pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud, std::vector<vector<int> > clusters_points_idx);
 void
 showColoredClusters(string fileoutpath_name, pcl::PointCloud<pcl::PointXYZRGB>::Ptr colored_cloud);
-void
-showColoredPolygons(string fileoutpath_name, const pcl::PolygonMesh::Ptr colored_polygons);
-
-
-
-//IO::save segmented clusters
-void
-saveSegmentedClusters(string path_name, pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud,std::vector<vector<int> > clusters_points_idx, int min_points);
-
-//IO::save gray image of the graphical model
-void
-saveGraphImage(string path_name, Eigen::MatrixXf graph_matrix);
 
 //IO::read & save task file
 std::vector<std::string>
 inputTaskTxtFile(string pathname_file);//Read the task file
-
-void
-outputResultTxtFile(string pathname_file, std::vector<float> input_vector);//Output the results txt file
-
-void
-outputFeaturesTxtFile(string pathname_file, std::vector<std::vector<float>> input_vector);//Output the txt file recording the object features
-
